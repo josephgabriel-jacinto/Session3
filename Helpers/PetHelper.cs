47ced98c9fd165657ab49abc3ae73c0039800af7
Joseph Gabriel Jacinto <2041504@cognizant.com>
2023-03-16 12:57:22 +0800

    added helper sstructure

---
+++ Helpers/PetHelper.cs
@@ -0,0 +1,30 @@
+﻿using ApiAutomationSession3.DataModels;
+using ApiAutomationSession3.Resources;
+using ApiAutomationSession3.Tests.GenerateTestData;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiAutomationSession3.Helpers
+{
+    public class PetHelper
+    {
+        public static async Task<Pet> AddNewPet(RestClient restClient)
+        {
+            var newPetData = GeneratePet.CreateNewPetData();
+            var postRequest = new RestRequest(ApiEndPoint.PostPet());
+
+            postRequest.AddJsonBody(newPetData);
+
+            var postResponse = await restClient.ExecutePostAsync<Pet>(postRequest);
+
+            var createdPetData = newPetData;
+
+            return createdPetData;
+        }
+
+    }
+}
