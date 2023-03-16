200d2927d8e363e72fda4c5a0d20d70a5c3f27e5
Joseph Gabriel Jacinto <2041504@cognizant.com>
2023-03-16 12:57:43 +0800

    Added resources structure

---
+++ Resources/ApiEndPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiAutomationSession3.Resources
+{
+    public class ApiEndPoint
+    {
+
+        public const string baseUrl = "https://petstore.swagger.io/v2";
+
+        public readonly string PetEndpoint = "pet";
+        
+        public static string GetPetById(long id) => $"{baseUrl}/pet/{id}";
+
+        public static string PostPet() => $"{baseUrl}/pet";
+
+        public static string DeletePetById(long id) => $"{baseUrl}/pet/{id}";
+
+    }
+}
