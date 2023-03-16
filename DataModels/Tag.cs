82300a468d603c941a33c38285297ce5532e08da
Joseph Gabriel Jacinto <2041504@cognizant.com>
2023-03-16 12:56:56 +0800

    Added DataModels folder

---
+++ DataModels/Tag.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiAutomationSession3.DataModels
+{
+    public class Tag
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+    }
+}
